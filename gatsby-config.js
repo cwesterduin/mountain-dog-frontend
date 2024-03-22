adee274b12c1668f65c4b1b9840d666dcd00a1a5
chris <chriswesterduin@outlook.com>
2024-03-22 20:34:41 +0000

    build

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,12 +37,12 @@ module.exports = {
                 name: "Mountain Dog",
                 short_name: "Mountain Dog",
                 start_url: "/",
-                background_color: "#12120f",
-                theme_color: "#fff",
+                background_color: "#ffffff",
+                theme_color: "#91adca",
                 // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
                 // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
                 display: "standalone",
-                icon: "images/icon.jpg", // This path is relative to the root of the site.
+                icon: "images/logo.png", // This path is relative to the root of the site.
                 // An optional attribute which provides support for CORS check.
                 // If you do not provide a crossOrigin option, it will skip CORS for manifest.
                 // Any invalid keyword or empty string defaults to `anonymous`
